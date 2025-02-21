63137b7b7cd50032d84732d312946c6eef471d0a
P Nitish <nitishnaik2022@gmail.com>
2025-02-21 11:56:51 +0530

    Update exp_5i.sol

--- exp_5i.sol
+++ exp_5i.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.19;
+
+contract Counter {
+    int public counter;
+
+    constructor() {
+        counter = 0;
+    }
+    function increment() public {
+        counter += 1;
+    }
+
+    function decrement() public {
+        counter -= 1;
+    }
+
+    function getCounter() public  view  returns (int) {
+        return  counter;
+    }
+}
